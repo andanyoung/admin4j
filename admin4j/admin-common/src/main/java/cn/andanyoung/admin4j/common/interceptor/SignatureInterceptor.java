2f693ea718ce36bdfa252941da3dd03439121761
andyoung <1218853253@qq.com>
2021-01-27 01:20:30 +0800

    接口签名

--- admin4j/admin-common/src/main/java/cn/andanyoung/admin4j/common/interceptor/SignatureInterceptor.java
+++ admin4j/admin-common/src/main/java/cn/andanyoung/admin4j/common/interceptor/SignatureInterceptor.java
@@ -2,13 +2,16 @@ package cn.andanyoung.admin4j.common.interceptor;
 
 import cn.andanyoung.admin4j.common.signature.SignatureUtil;
 import cn.andanyoung.admin4j.common.signature.annotation.SignatureAccess;
+import cn.andanyoung.admin4j.common.utils.SpringContextUtil;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  * 签名拦截器
@@ -17,19 +20,28 @@ import javax.servlet.http.HttpServletResponse;
 @Order(1)
 public class SignatureInterceptor implements HandlerInterceptor {
 
+  final static String SIGNATURE_WHITE_LIST_BEN_KEY = "signature_white_list";
+
+  AntPathMatcher antMatchers = new AntPathMatcher();
+  // private final Map<String, String[]>  =new CopyOnWriteArraySet<>(256);
+
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
           throws Exception {
 
-    // AntPathMatcher
-    // 认证过的地址，需要签名，没有认证的（忽略）在这里也忽略
-    //    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    String requestURI = request.getRequestURI();
+
+    if (isInWhiteList(requestURI)) {
+      return false;
+    }
+
     HandlerMethod handlerMethod = (HandlerMethod) handler;
+
     SignatureAccess annotation = handlerMethod.getMethod().getAnnotation(SignatureAccess.class);
 
     // 方法注解值为false
     if (annotation != null && !annotation.value()) {
-      return true;
+      return false;
     }
     SignatureAccess classAnnotation = handlerMethod.getClass().getAnnotation(SignatureAccess.class);
     if ((annotation != null && annotation.value())
@@ -39,10 +51,24 @@ public class SignatureInterceptor implements HandlerInterceptor {
               request, getAppSecretByAppId(request.getHeader(SignatureUtil.DEFAULT_APPID_FIELD)));
     }
 
-    return true;
+    return false;
   }
 
   public String getAppSecretByAppId(String appid) {
     return appid;
+  }
+
+  Boolean isInWhiteList(String uri) {
+    Object signatureWhiteListConfig = SpringContextUtil.getBean(SIGNATURE_WHITE_LIST_BEN_KEY);
+    if (signatureWhiteListConfig != null) {
+
+      List<String> signatureWhiteList = (List) signatureWhiteListConfig;
+      for (String pattern : signatureWhiteList) {
+        if (antMatchers.match(pattern, uri)) {
+          return true;
+        }
+      }
+    }
+    return false;
   }
 }
