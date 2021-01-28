cb1f4eda2c4017bbcfde08d0489352940d5688db
andyoung <1218853253@qq.com>
2021-01-29 00:53:11 +0800

    接口签名-优化

--- admin4j/admin-common/src/main/java/cn/andanyoung/admin4j/common/signature/SignatureUtil.java
+++ admin4j/admin-common/src/main/java/cn/andanyoung/admin4j/common/signature/SignatureUtil.java
@@ -2,7 +2,6 @@ package cn.andanyoung.admin4j.common.signature;
 
 import cn.andanyoung.admin4j.common.response.enums.ResponseEnum;
 import cn.andanyoung.admin4j.common.utils.BodyReaderHttpServletRequestWrapper;
-import cn.andanyoung.admin4j.common.utils.HttpRequestUtils;
 import cn.andanyoung.admin4j.common.utils.ResponseUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
@@ -31,21 +30,29 @@ public class SignatureUtil {
     // 防止流读取一次后就没有了, 所以需要将流继续写出去
     HttpServletRequest requestWrapper = new BodyReaderHttpServletRequestWrapper(request);
 
-    TreeMap<String, String> otherParam = new TreeMap<>();
-    otherParam.put(DEFAULT_Authorization_FIELD, request.getHeader(DEFAULT_Authorization_FIELD));
-    otherParam.put(DEFAULT_APPID_FIELD, request.getHeader(DEFAULT_APPID_FIELD));
+    String signField = requestWrapper.getHeader(DEFAULT_SIGN_FIELD);
+    if (StringUtils.isEmpty(signField)) {
+
+      ResponseUtil.sendJSONResponse(ResponseEnum.SIGN_AUTH_TIME);
+      return false;
+    }
+
     String timestamp = request.getHeader(DEFAULT_TIMESTAMP_FIELD);
-
-
     if (StringUtils.isEmpty(timestamp) || System.currentTimeMillis() - Long.parseLong(timestamp) > MAX_INTERNAL) {
       // 间隔时间 超过 MAX_INTERNAL////
       ResponseUtil.sendJSONResponse(ResponseEnum.SIGN_AUTH_TIME);
       return false;
     }
-    otherParam.put(DEFAULT_TIMESTAMP_FIELD, timestamp);
 
-    SortedMap<String, String> allParams = HttpRequestUtils.getAllParams(request, otherParam);
-    return verifySign(allParams, requestWrapper.getHeader(DEFAULT_SIGN_FIELD), appSecret);
+
+    Map<String, String[]> parameterMap = request.getParameterMap();
+    parameterMap.put(DEFAULT_Authorization_FIELD, new String[]{request.getHeader(DEFAULT_Authorization_FIELD)});
+    parameterMap.put(DEFAULT_APPID_FIELD, new String[]{request.getHeader(DEFAULT_APPID_FIELD)});
+    parameterMap.put(DEFAULT_TIMESTAMP_FIELD, new String[]{timestamp});
+
+    SortedMap<String, String[]> sortedMap = new TreeMap<>(parameterMap);
+
+    return verifySign(sortedMap, signField, appSecret);
   }
 
   /**
@@ -53,13 +60,10 @@ public class SignatureUtil {
    * @return 验证签名结果
    */
   private static boolean verifySign(
-          SortedMap<String, String> params, String urlSign, String appSecret) {
+          SortedMap<String, String[]> params, String urlSign, String appSecret) {
 
-    if (params == null || StringUtils.isEmpty(urlSign)) {
-      return false;
-    }
     // 把参数加密
-    String paramsSign = getParamsSign(params, appSecret);
+    String paramsSign = calculateParamsSign(params, appSecret);
     return !StringUtils.isEmpty(paramsSign) && urlSign.equals(paramsSign);
   }
 
@@ -67,17 +71,25 @@ public class SignatureUtil {
    * @param params 所有的请求参数都会在这里进行排序加密
    * @return 得到签名
    */
-  private static String getParamsSign(SortedMap<String, String> params, String appSecret) {
-    // 要先去掉 Url 里的 Sign
+  private static String calculateParamsSign(SortedMap<String, String[]> params, String appSecret) {
 
-    ArrayList<String> strings = new ArrayList<>();
-    Set<Map.Entry<String, String>> entry = params.entrySet();
-    for (Map.Entry<String, String> temp : entry) {
-      strings.add(temp.getKey() + "=" + temp.getValue());
+    StringBuilder stringBuilder = new StringBuilder();
+
+    Set<Map.Entry<String, String[]>> entry = params.entrySet();
+    for (Map.Entry<String, String[]> temp : entry) {
+      String[] values = temp.getValue();
+      if (values.length > 1) {
+        Arrays.sort(values);
+      }
+      for (String value : values) {
+        stringBuilder.append(temp.getKey());
+        stringBuilder.append("=");
+        stringBuilder.append(value);
+        stringBuilder.append("&");
+      }
     }
 
-    String paramsJsonStr = org.apache.commons.lang3.StringUtils.join(strings, "&") + appSecret;
-    log.debug(paramsJsonStr);
-    return DigestUtils.md5DigestAsHex(paramsJsonStr.getBytes()).toUpperCase();
+    stringBuilder.append(appSecret);
+    return DigestUtils.md5DigestAsHex(stringBuilder.toString().getBytes()).toUpperCase();
   }
 }
